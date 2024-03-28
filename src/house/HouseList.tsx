e97eaeb17e2f5146b00f6cb35708ed02e07f4255
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-03-28 09:26:48 -0500

    Save

--- src/house/HouseList.tsx
+++ src/house/HouseList.tsx
@@ -3,11 +3,22 @@
 import { currencyFormatter } from "../config";
 import { useFetchHouses } from "../hooks/HouseHooks";
 import ApiStatus from "../apiStatus";
+import { useNavigate } from "react-router-dom";
+import { House } from "../types/house";
 
 //import { House } from "../types/house";
 //import config from "../config";
 
 const HouseList = () => {
+
+  const nav = useNavigate();  //navclick. This is a Hook that returns a 
+             //function that takes a string with a URL to navigate to.
+             //Note: useNavigate() will NOT create a request to the server.
+             //      it will use the internal routing system.
+             //A normal redirect using window location ref for example will 
+             //work too but the entire application will be RELOADED from the 
+             //server after which the routing mechanism will determine the 
+             //RIGHT component to dispaly
   //This hook returns "data" so we need to destructure it
   const {data, status, isSuccess} = useFetchHouses(); 
   
@@ -29,8 +40,9 @@ const HouseList = () => {
           </tr>
         </thead>
         <tbody>
-            {data && data.map((h) => (
-              <tr key={h.id}>
+        {data &&
+            data.map((h: House) => (
+              <tr key={h.id} onClick={() => nav(`/house/${h.id}`)}>
                 <td>{h.address}</td>
                 <td>{h.country}</td>
                 <td>{currencyFormatter.format(h.price)}</td>
